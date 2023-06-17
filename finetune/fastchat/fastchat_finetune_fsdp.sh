f1156676df03c0c242b891c1bbab037db4494031
tafflan <3219599757@qq.com>
2023-06-17 03:56:03 +0000

    Add one click finetune script.

---
+++ finetune/fastchat/fastchat_finetune_fsdp.sh
@@ -0,0 +1,24 @@
+nohup torchrun --nproc_per_node=3 --master_port=$RANDOM /workspace/code/fastchat/train/train_mem.py \
+    --model_name_or_path yahma/llama-7b-hf  \
+    --data_path /workspace/code/data/dummy_conversation.json \
+    --bf16 True \
+    --output_dir /workspace/output/vicuna_7b_dummy/ \
+    --num_train_epochs 3 \
+    --per_device_train_batch_size 8 \
+    --per_device_eval_batch_size 8 \
+    --gradient_accumulation_steps 4 \
+    --evaluation_strategy "no"\
+    --save_strategy "steps"\
+    --save_steps 100 \
+    --save_total_limit 10 \
+    --learning_rate 2e-5 \
+    --weight_decay 0. \
+    --warmup_ratio 0.03 \
+    --lr_scheduler_type "cosine"\
+    --logging_steps 1 \
+    --model_max_length 512 \
+    --gradient_checkpointing True \
+    --lazy_preprocess True\
+    --fsdp "full_shard auto_wrap offload" \
+    --fsdp_transformer_layer_cls_to_wrap 'LlamaDecoderLayer' \
+    --tf32 True & 
